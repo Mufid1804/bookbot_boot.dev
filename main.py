ce11797834935b54d3840553154ff129ebcfa86f
Muhammad Mufid <m.mufid1804@gmail.com>
2024-04-30 14:09:51 +0700

    Improve it to OOP

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = read_book(book_path)
+    book = Book(book_path)
 
     print(f"--- Begin report of {book_path} ---")
-    print(f"{count_words(text)} words found in the document\n")
+    print(f"{book.word_count} words found in the document\n")
 
-    char_counts = get_char_counts(text)
+    char_counts = book.char_freq
     sorted_char_counts = sorted(char_counts.items(), key=lambda x: x[1], reverse=True)
 
     for char, count in sorted_char_counts:
-        print(f"The '{char}' character was found {count} times")
+        print(f"The '{char}' char was found {count} times")
 
     print("--- End report ---")
 
 
-def read_book(book_path):
-    with open(book_path) as f:
-        return f.read()
+class Book:
+    def __init__(self, path):
+        self.path = path
+        self.text_content = self._read_book()
+        self.word_count = self._count_words()
+        self.char_freq = self._get_char_freq()
 
+    def _read_book(self):
+        with open(self.path) as f:
+            return f.read()
 
-def get_char_counts(text):
-    char_counts = {}
-    for char in text.lower():
-        if char.isalpha():
-            char_counts[char] = char_counts.get(char, 0) + 1
-    return char_counts
+    def _count_words(self):
+        return len(self.text_content.split())
 
-
-def count_words(text):
-    return len(text.split())
+    def _get_char_freq(self):
+        char_counts = {}
+        for char in self.text_content.lower():
+            if char.isalpha():
+                char_counts[char] = char_counts.get(char, 0) + 1
+        return char_counts
 
 
 if __name__ == "__main__":
